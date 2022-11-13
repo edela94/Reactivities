643971235fdb9cc1065f7cd5eb8f19fdda110ded
Esteban de la Torre <edelatorre94@gmail.com>
2022-11-12 22:06:09 -0800

    Add uuid to created activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import Navbar from "./Navbar";
 import ActityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -43,7 +44,7 @@ function App() {
           ...activities.filter((x) => x.id !== activity.id),
           activity,
         ])
-      : setActivities([...activities, activity]);
+      : setActivities([...activities, { ...activity, id: uuid() }]);
     setEditMode(false);
     setSelectedActivity(activity);
   };
