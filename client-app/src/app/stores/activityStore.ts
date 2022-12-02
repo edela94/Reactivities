e5673249b971f50138ffb455e773ecff950d4fe3
Esteban de la Torre <edelatorre94@gmail.com>
2022-12-01 22:42:19 -0800

    Add mobx store

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,13 @@
+import { makeAutoObservable } from "mobx";
+
+export default class ActivityStore {
+  title = "hello from mobx";
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setTitle = () => {
+    this.title = this.title + "!";
+  };
+}
